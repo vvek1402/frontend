d64ac0e9fdfce86a387ba2583240c493964f158c
vivek <vivekmoradiya1402@gmail.com>
2024-05-02 01:12:23 +0530

    mobile-ment

--- Theme 1 : Course/main.js
+++ Theme 1 : Course/main.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element to add the class to
-    const element = document.getElementsByClassName('navbar')[0];
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 
-    // Function to add class when scrolling
-    function addClassOnScroll() {
-        if (window.scrollY > 100) {
-            element.classList.add('scrolled');
-        } else {
-            element.classList.remove('scrolled');
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.getElementsByClassName("navbar")[0];
+
+  function addClassOnScroll() {
+    if (window.scrollY > 100) {
+      element.classList.add("scrolled");
+      mobileNav.classList.add("scrolled");
+    } else {
+      element.classList.remove("scrolled");
+      mobileNav.classList.remove("scrolled");
     }
+  }
 
-    // Add scroll event listener
-    window.addEventListener('scroll', addClassOnScroll);
+  // Add scroll event listener
+  window.addEventListener("scroll", addClassOnScroll);
 });
+
+menuIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.add("active");
+  });
+  
+  closeIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.remove("active");
+  });
