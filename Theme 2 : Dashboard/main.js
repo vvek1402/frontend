5af8e278c28fae723033b130e6f9dace109274c9
vivek <vivekmoradiya1402@gmail.com>
2024-05-03 13:20:31 +0530

    dashboard completed

---
+++ Theme 2 : Dashboard/main.js
@@ -0,0 +1,39 @@
+let ctx = document.getElementById("DoughnutChart").getContext("2d");
+let myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [
+      {
+        backgroundColor: [
+          "#2ecc71",
+          "#3498db",
+          "#95a5a6",
+          "#9b59b6",
+          "#f1c40f",
+          "#e74c3c",
+          "#34495e",
+        ],
+        data: [12, 19, 3, 17, 28, 24, 7],
+      },
+    ],
+  },
+});
+
+const labels = ["M", "T", "W", "T", "F", "S", "S"];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+let linectx = document.getElementById("lineChart").getContext("2d");
+let lineChart = new Chart(linectx, {
+  type: "line",
+  data: data
+});
